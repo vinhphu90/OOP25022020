2f8215727c78a63f9496f4f1e89c6968c161502a
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-04-02 00:05:03 +0700

    object and class

--- app/src/main/java/com/examplevinhphutvp/oop25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/oop25022020/MainActivity.java
@@ -3,6 +3,7 @@ package com.examplevinhphutvp.oop25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Object va class
+//        String a = "5";
+        // function construrtor
+        Person nguyenvana = new Person();
+        nguyenvana.age = 10;
+        nguyenvana.name = "Nguyen Van A";
+        Log.d("BBB", nguyenvana.name + " " + nguyenvana.age);
+
     }
 }
---
+++ app/src/main/java/com/examplevinhphutvp/oop25022020/Person.java
@@ -0,0 +1,8 @@
+package com.examplevinhphutvp.oop25022020;
+
+public class Person {
+    // Thuộc tính
+    String name;
+    int age;
+    // Hành vi
+}
